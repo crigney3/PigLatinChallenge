22a6508280200d706b8894266284347f07917e9b
Corey Rigney <coreyrigney@gmail.com>
2018-12-10 00:03:46 -0500

    First commit, did it all in one go!

---
+++ PigLatinTranslator/PigLatinTranslator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatinTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Translator instance and main loop
+            Translator translator = new Translator();
+            bool quit = false;
+            while (!quit)
+            {
+                Console.WriteLine("1 - Translate some pig latin!\n2 - Quit");
+                string input = Console.ReadLine().Trim().ToLower();
+                switch (input)
+                {
+                    case "1":
+                    case "translate":
+                        //Get new user input and pig latin it
+                        Console.WriteLine("Enter what you want turned into pig latin:");
+                        string toBeLatined = Console.ReadLine().Trim().ToLower();
+                        
+                        //In case it's a sentence, .Split it
+                        string[] sentenceArray = toBeLatined.Split(' ');
+                        string[] outputArray = new string[sentenceArray.Length];
+                        int count = 0;
+                        foreach(string word in sentenceArray)
+                        {
+                            outputArray[count] = translator.ToPigLatin(word);
+                            count++;
+                        }
+
+                        //Put the sentence back together
+                        foreach(string outputWord in outputArray)
+                        {
+                            Console.Write(outputWord + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case "2":
+                    case "quit":
+                        quit = !quit;
+                        Console.WriteLine("Quitting...");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ PigLatinTranslator/PigLatinTranslator/Translator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatinTranslator
+{
+    class Translator
+    {
+        //Default constructor
+        public Translator()
+        {
+            //Nothing needed here
+        }
+
+        //Private method to determine whether the word starts with a vowel
+        private bool StartsWithVowel(string word)
+        {
+            char[] vowels = new char[5];
+            vowels[0] = 'a';
+            vowels[1] = 'e';
+            vowels[2] = 'i';
+            vowels[3] = 'o';
+            vowels[4] = 'u';
+
+            //Loop checks each vowel
+            for(int i = 0; i < 5; i++)
+            {
+                if(word.ToArray()[0] == vowels[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int FirstVowel(string word)
+        {
+            char[] vowels = new char[5];
+            vowels[0] = 'a';
+            vowels[1] = 'e';
+            vowels[2] = 'i';
+            vowels[3] = 'o';
+            vowels[4] = 'u';
+
+            //Loop checks every letter for being a vowel, then returns index
+            for (int i = 0; i < word.Length; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if (word.ToArray()[i] == vowels[j])
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return 1;
+        }
+
+        //Public method to change the word to pig latin, determining which logic to use from StartsWithVowel and FirstVowel
+        public string ToPigLatin(string word)
+        {
+            word = word.Trim().ToLower();
+            if (StartsWithVowel(word))
+            {
+                //Simple, just add 'yay'
+                return word + "yay";
+            }
+            else
+            {
+                //Take the word apart and make a temporary array for storage
+                char[] wordArray = word.ToArray();
+                char[] tempArray = new char[word.Length];
+
+                int count = 0;
+                //Loop that fills the temparray starting at the first vowel
+                for(int i = FirstVowel(word); i < word.Length; i++)
+                {
+                    tempArray[count] = wordArray[i];
+                    count++;
+                }
+
+                //Find out how many letters there were before the vowel, and loop that many times to add them to the end
+                int moveToEnd = word.Length - (word.Length - FirstVowel(word));
+                for(int i = 0; i < moveToEnd; i++)
+                {
+                    tempArray[count] = wordArray[i];
+                    count++;
+                }
+
+                //Put the word back together, and add the extra stuff
+                string pigLatinWord = "";
+                for(int i = 0; i < tempArray.Length; i++)
+                {
+                    pigLatinWord += tempArray[i];
+                }
+                return pigLatinWord + "ay";
+            }
+        }
+    }
+}
